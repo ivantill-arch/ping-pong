c358049d2030dbbae0f42174e324b90aa77ce3ae
ivantill-arch <slaninak@list.ru>
2025-01-11 16:05:41 +0300

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -8,40 +8,37 @@ window.fill(back)
 clock = time.Clock()
 FPS = 60
 game = True
+finish = False
 
-class Area():
-    def __init__(self, x=0, y=0, widht=10, height=10, color=None):
-        self.rect = pygame.Rect(x, y, widht, height)
-        self.fill_color = back
-        if color:
-            self.color = color
 
-    def set_color(self, new_color):
-        self.fill_color = new_color
-
-    def painting(self):
-        pygame.draw.rect(window, self.fill_color, self.rect)
-
-    def collidepoint(self, x, y):
-        return self.rect.collidepoint(x, y)
-
-    def colliderect(self, rect):
-        return self.rect.colliderect(rect)
-
-class Picture(Area):
-    def __init__(self, name_image, x=0, y=0, widht=10, height=10):
-        Area.__init__(self, x=x, y=y, widht=widht, height=height, color=None)
-        self.image = pygame.image.load(name_image)
-    
-    def draw(self):
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-
+class Ball(GameSprite):
+    def update(self):
+        ball.player_x += self.speed
+        ball.player_y += self.speed
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if finish != True:
+        window.blit((0,0))
+
+    if keys_pressed[K_LEFT] and player.rect.y >30:
+        player.rect.y -= speed
+    if keys_pressed[K_RIGHT] and player.rect.y < 595:
+        player.rect.y += speed    
 
     display.update()
     clock.tick(FPS)
